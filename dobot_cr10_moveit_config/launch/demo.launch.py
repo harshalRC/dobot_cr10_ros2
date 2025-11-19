7234434f36c96e47164f93c8d15e5262aa73d056
Harshal Salian <harshal22.hs@gmail.com>
2025-11-19 14:27:05 -0500

    launch edit

--- dobot_cr10_moveit_config/launch/demo.launch.py
+++ dobot_cr10_moveit_config/launch/demo.launch.py
@@ -6,47 +6,23 @@ from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
-    # Declare launch arguments
-    declared_arguments = []
-    declared_arguments.append(
+    # Declare argument
+    declared_arguments = [
         DeclareLaunchArgument(
             "use_fake_hardware",
             default_value="true",
-            description="Start robot with fake hardware mirroring command to its states.",
+            description="Use fake hardware (true) or real robot (false)",
         )
-    )
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            "robot_ip",
-            default_value="192.168.5.1",
-            description="IP address of the robot",
-        )
-    )
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            "robot_port",
-            default_value="29999",
-            description="Port number of the robot",
-        )
-    )
-
-    # Get launch arguments
+    ]
+    
+    # Get argument value
     use_fake_hardware = LaunchConfiguration("use_fake_hardware")
-    robot_ip = LaunchConfiguration("robot_ip")
-    robot_port = LaunchConfiguration("robot_port")
-
-    # Build MoveIt config with mappings
+    
+    # Build config
     moveit_config = (
         MoveItConfigsBuilder("dobot_cr10", package_name="dobot_cr10_moveit_config")
-        .robot_description(
-            mappings={
-                "use_fake_hardware": use_fake_hardware,
-                "robot_ip": robot_ip,
-                "robot_port": robot_port,
-            }
-        )
+        .robot_description(mappings={"use_fake_hardware": use_fake_hardware})
         .to_moveit_configs()
     )
-
+    
     return LaunchDescription(declared_arguments + [generate_demo_launch(moveit_config)])
-
